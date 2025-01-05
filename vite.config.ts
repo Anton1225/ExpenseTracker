3e8d3678e8ffac79e035ac61ebe2715d914d8ac6
Anton1225 <antongusev1225@gmail.com>
2025-01-05 15:34:38 +0300

    minor changes in package.json and vite.config

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,8 @@
 import { defineConfig } from 'vite'
-import { fileURLToPath } from 'url'
-
 import react from '@vitejs/plugin-react'
 
-const __dirname = fileURLToPath(new URL('.', import.meta.url))
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/https://github.com/Anton1225/ExpenseTracker/' : '/',
   plugins: [react()],
-  root: __dirname,
+  base: "/ExpenseTracker/"
 })
